7b0b9ffe6ef4c8a8ee9daf421db09cd2b9e50191
MarcOreliOoo <albeaurelien@gmail.com>
2024-03-07 15:59:00 +0100

    add prettier conf + height screen

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { ThemeToogle } from "../components/theme-toogle";
 
 const Navbar = () => {
-	return (
-		<div className="w-full h-16 flex justify-end items-center mx-auto px-2">
-			<ThemeToogle />
-		</div>
-	);
+    return (
+        <div className="mx-auto flex h-16 w-full items-center justify-end px-2">
+            <ThemeToogle />
+        </div>
+    );
 };
 
 export default Navbar;
